42a6dcdc61bbbdf0b1f6ae09e8c2f3e9c86893d8
Nathan-Simonnet <nathan.simonnet@gmail.com>
2024-09-10 17:19:05 +0200

    Header done and responsive. Hamburger librairie added to project.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,35 @@
 console.log("test")
+
+document.querySelector('.hamburger').addEventListener('click', function () {
+
+    this.classList.toggle('is-active');
+    const ul = document.querySelector('header>nav>ul');
+    ul.classList.toggle("open");
+
+    // Allow transition only for th hamburger state
+    ul.style.transition = "0.3s"
+    setTimeout(() => {
+        ul.style.transition = "none"
+    }, 500)
+
+    ul.addEventListener('click', () => {
+        ul.classList.remove("open");
+    });
+
+    window.addEventListener('scroll', () => {
+        ul.classList.remove("open");
+        this.classList.remove('is-active');
+    });
+    
+});
+
+//    // Prevent hamburger alteration for keyboard users
+//    document.addEventListener('keydown', function (event) {
+//     const ul = document.querySelector('header>nav>ul');
+//     if (event.key === 'Enter' && ul.classList.contains("open")) {
+//         ul.classList.remove("open");
+//         document.querySelector('.hamburger').classList.remove('is-active');
+//         console.log(ul.classList.contains("open"))
+//     }
+// });
+
